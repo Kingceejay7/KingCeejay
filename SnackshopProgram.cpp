b3c70507377883cb1e41dd15ddce00ff4219a35f
Clinton Joel Kamau <108893123+Kingceejay7@users.noreply.github.com>
2022-07-14 12:48:19 +0300

    Add files via upload

---
+++ SnackshopProgram.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int menu ();
+int main ()
+}
+printf("Hellow there welcome to your favourite snack shop!\n");
+menu () 
+return 0;
+{
+int menu () {
+int item;
+printf("1.Hot Dog-Kes 90\n");
+printf("2.Hamburger-kes 300\n");
+printf("3.French Fries-kes 150\n");
+printf("4.Milkshake-kes150\n");
+printf("5.Coffee-kes 70\n");
+printf("6.Beef Smokie-kes 40\n");
+printf("Enter your Snack Number:");
+scanf("%d",& item);
+{
+if ( item >0 && item <= 6{
+    printf("your items were added to cart\n");
+{
+else {
+    printf("invalid\n");
+    menu();
+{
+return item ;
+
+{
